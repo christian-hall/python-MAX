9eeae29ba881c4fd1e7da3e5af8e3832604978e5
Christian Hall <christiannealhall@gmail.com>
2021-04-24 10:46:21 -0400

    added printout formatting to remove extra zeroes

--- ch03/temp-converter/main.py
+++ ch03/temp-converter/main.py
@@ -4,7 +4,7 @@ leave = 'n'
 while leave.lower() != 'y':
     fahrenheit = float(input('Enter degrees in Fahrenheit: '))
     celsius = (fahrenheit - 32) * (5 / 9)
-    print('Degrees in Celsius: ', round(celsius, 2))
+    print('Degrees in Celsius: ', '%g' % (round(celsius, 2)))
     print()
     leave = input('Quit? (y/n): ')
     print()
