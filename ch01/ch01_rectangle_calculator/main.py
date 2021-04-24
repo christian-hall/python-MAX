61bd7ee780d79b8df9d0c2f6dd1d0dbee5770828
Christian Hall <christiannealhall@gmail.com>
2021-04-24 09:36:46 -0400

    completed first lab assignment

---
+++ ch01/ch01_rectangle_calculator/main.py
@@ -0,0 +1,15 @@
+print('Welcome to the Area and Perimeter Calculator')
+print()
+choice = 'n'
+while choice.lower() != 'y':
+    length = int(input('enter length: '))
+    width = int(input('enter width:  '))
+    area = length * width
+    perimeter = length * 2 + width * 2
+    print(f'Area:         {area}')
+    print(f'Perimeter:    {perimeter}')
+    print()
+    choice = input('Quit? (y/n): ')
+    print()
+
+print('Goodbye')
