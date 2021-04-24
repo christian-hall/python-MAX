71ab733e45c89df6582b1060d55e675f5416157d
Christian Hall <christiannealhall@gmail.com>
2021-04-24 15:07:06 -0400

    added chapter 8

--- ch08/ch08/main.py
+++ ch08/ch08/main.py
@@ -16,4 +16,61 @@ middle_name = name[idx_1+1:idx_2]
 print(middle_name)
 last_name = name[idx_2 + 1: len(name)]
 print(last_name)
+# add a space between each character
+for character in name:
+    print(character, end=' ')
+    print()
 
+# processing lists
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print('first letter', letters[0])
+print('last letter', letters[-1])
+
+# slicing
+print('2nd through 4th letters', letters[1:4])
+
+# replacing an element
+letters[4] = 'z'
+print(letters)
+letters.append('h')
+print(letters)
+
+# remove using pop and pop
+letters.pop(-1)
+print(letters)
+letters.remove('z')
+print(letters)
+letters.insert(4, 'e')
+print(letters)
+
+# for ____ in ____
+for letter in letters:
+    print(letter, end=" ")
+print()
+
+# count function
+letters = ['a', 'b', 'a', 'd', 'd', 'f', 'g']
+print(letters)
+print('number of As:', letters.count('a'))
+
+# range
+print('range:', range(1, 10))
+print('range 0 list:', list(range(1, 10)))
+
+# for using index
+for i in range(0, len(letters)):
+    print(f'letter {i}: {letters[i]}')
+
+# does our letters list contain 'c'?
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+if 'b' in letters:
+    print('B has been found')
+
+# use * in a string
+str1 = 'a' * 10
+print(str1)
+
+letters = letters * 2
+print(letters)
+
+# a tuple is an immutable list, uses ( ) instead of [ ]
