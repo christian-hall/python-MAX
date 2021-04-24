3ad522298e16e4deae0fe41007ed1e618bc8b71a
Christian Hall <christiannealhall@gmail.com>
2021-04-24 09:28:10 -0400

    update

--- ch01/hello_world/main.py
+++ ch01/hello_world/main.py
@@ -1,3 +1,5 @@
+import sys
+
 print('Hello World')
 
 # code completion and syntax error detection
@@ -26,3 +28,26 @@ print('a', 'b', 'c', 'd', sep='-')
 days_in_week = 7
 print('there are ' + str(days_in_week) + ' days in a week')
 print(f'There are {days_in_week} days in a week')
+
+# getting user input
+name = input('What is your name? ')
+print(f'Hello, {name}!')
+# anytime you use that input it interprets it as a string
+birth_month = int(input('What is the number of your birth month? '))
+print(type(birth_month))
+
+# sys.argc. usage - this is the location of the file
+# this is where those imports come in
+print(sys.argv[0])
+
+if birth_month == 3:
+    print('March selected')
+elif birth_month == 4:
+    print('April selected')
+else:
+    print('Other months selected')
+
+choice = 'y'
+while choice == 'y':
+    print('y entered')
+    choice = input('Try again? ')
