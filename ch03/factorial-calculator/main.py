eab392a884d43e77a5ea106bb19ec14932c8102c
Christian Hall <christiannealhall@gmail.com>
2021-04-24 11:18:11 -0400

    added ch05

--- ch03/factorial-calculator/main.py
+++ ch03/factorial-calculator/main.py
@@ -1,7 +1,7 @@
 print('Welcome to the Factorial Calculator!')
 leave = 'n'
 while leave.lower() != 'y':
-    factorial, response = 1, int(input('Enter an integer greater than 0 and less than 10: '))
+    factorial, response = 1, int(input('Enter an integer greater than 0: '))
     for i in range(1, response + 1):
         factorial *= i
     print(f'The factorial of {response} is {factorial}.')
---
+++ ch05/ch05/main.py
@@ -0,0 +1,9 @@
+# Chapter 5 - functions and exceptions
+
+# keyword 'def' defines a function
+def print_something():
+    print('something')
+
+
+# call the function below the method
+print_something()
